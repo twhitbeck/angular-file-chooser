2dd5dd4ad26c9ec06f55cbec8a89d232fffe81fd
Tim Whitbeck <twhitbeck@gmail.com>
2014-08-08 16:25:01 -0400

    v0.0.1

---
+++ angular-file-chooser.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  angular.module('tw.directives.fileChooser', []).directive('twFileChooser', ['$compile', '$document', '$http', '$templateCache', '$window', function($compile, $document, $http, $templateCache, $window) {
+    return {
+      restrict: 'A',
+      link: function twFileChooserPostLink(scope, el, attrs) {
+        // Preconditions
+        if (element.nodeName.toLowerCase() !== 'input' || !attrs.type || attrs.type.toLowerCase() !== 'file' || !attrs.twFileChooser) {
+          return;
+        }
+
+        var input = el[0];
+        var current = el;
+
+        scope.choose = function choose() {
+          input.dispatchEvent(new $window.MouseEvent('click'));
+        };
+
+        scope.$watch(attrs.twFileChooser, function(newVal, oldVal) {
+          if (!newVal) {
+            if (oldVal) {
+              // Reset
+              current.replaceWith(el);
+              current = el;
+            }
+
+            return;
+          }
+
+          $http.get(newVal, {cache: $templateCache}).then(function(response) {
+            var newElement = $compile(response.data)(scope);
+            current.replaceWith(newElement);
+            current = newElement;
+          }, function(response) {
+            // TODO: log error
+            current.replaceWith(el);
+            current = el;
+          });
+        });
+      }
+    };
+  }]);
+})();
